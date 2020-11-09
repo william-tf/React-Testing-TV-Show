24ef6842d0f7df0dab8f3f248f444a2f2b929b6b
will Fletcher <wfletch7@gmail.com>
2020-11-09 17:35:45 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-
+import {fetchShow} from './api/fetchShow'
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -15,23 +15,18 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
+    fetchShow()
+    .then(res => {
+        //console.log(res.data);
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+        })
+        .catch(err => console.log(err))
   }, []);
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
   };
-
   if (!show) {
     return <h2>Fetching data...</h2>;
   }
@@ -39,9 +34,10 @@ export default function App() {
   return (
     <div className="App">
       <img className="poster-img" src={show.image.original} alt={show.name} />
-      <h1>{show.name}</h1>
+      <h1 >{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
+        data-testid='dropdown'
         options={Object.keys(seasons)}
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
